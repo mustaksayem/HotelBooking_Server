1be59f38db84f308e762bc20917361d245b2ff46
Sayem <mustakhasansayem@gmail.com>
2024-05-13 20:47:13 +0600

    Review added in home page decending order

--- index.js
+++ index.js
@@ -34,11 +34,20 @@ async function run() {
     try {
         const roomCollection = client.db('HotelManagement').collection('Rooms')
         const bookRoomCollection = client.db('HotelManagement').collection('Booking')
+        const reviewCollection = client.db('HotelManagement').collection('Review')
 
         app.get('/rooms', async (req, res) => {
             const result = await roomCollection.find().sort({price_per_night:1}).toArray()    
             res.send(result)
           })
+        //   review 
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().sort({reviewDate:-1}).toArray()    
+            res.send(result)
+          })
+
+
+
 
           app.get('/roomdetails/:id', async (req, res) => {
             const id = req.params.id
@@ -46,12 +55,95 @@ async function run() {
             const result = await roomCollection.findOne(query)
             res.send(result);
         })
+
+        // dfghjk
+
+        app.get('/rooms/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await roomCollection.findOne(query)
+            res.send(result);
+        })
+
+
+// booking single id get
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await bookRoomCollection.findOne(query)
+            res.send(result);
+        })
+
+
+
+
+
+
+
         // save booking rooms in database
           app.post('/booking',async (req,res) =>{
             const bookingData = req.body
             const result = await bookRoomCollection.insertOne(bookingData)
             res.send(result)
           })
+
+        //   save review in database
+        app.post('/review',async (req,res) =>{
+            const reviewData = req.body
+       
+            const result = await reviewCollection.insertOne(reviewData)
+            res.send(result)
+            
+          })
+          
+           //update avalability
+           app.put("/rooms/:id", async (req, res) => {
+            const query = { _id: new ObjectId(req.params.id) }
+            const data = {
+                $set: {
+                    availability: req.body.availability,
+                 
+                }
+            }
+            console.log(data);
+            const result = await roomCollection.updateOne(query, data)
+            res.send(result)
+        })
+
+
+        // update date
+
+        app.put("/update/:id", async (req, res) => {
+            // Add console log here
+            console.log("Received PUT request for id:", req.params.id);
+          
+            const query = { _id: new ObjectId(req.params.id) };
+            const data = {
+              $set: {
+                bookingDate: req.body.bookingDate,
+              },
+            };
+            console.log(data);
+            const result = await bookRoomCollection.updateOne(query, data);
+            res.send(result);
+          });
+          
+
+
+
+          app.get('/mybookings/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await bookRoomCollection.find(query).toArray()
+            res.send(result);
+        })
+
+        app.delete('/mybooking/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await bookRoomCollection.deleteOne(query)
+            res.send(result)
+        })
     
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
